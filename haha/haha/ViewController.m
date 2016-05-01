9ff91a99dad516783be9986b1cc39053901b2e91
rss <runturss@163.com>
2016-05-01 23:58:48 +0800

    99

---
+++ haha/haha/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  haha
+//
+//  Created by 闰尚松 on 16/5/1.
+//  Copyright © 2016年 runshangsong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ haha/haha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  haha
+//
+//  Created by 闰尚松 on 16/5/1.
+//  Copyright © 2016年 runshangsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
